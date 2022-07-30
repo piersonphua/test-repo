63a514186fcec2a86b2de6bed6dcfd6d51138dca
piersonphua@hotmail.com <piersonphua@hotmail.com>
2022-07-30 11:04:32 +0800

    add in print(hello)

--- formatting.py
+++ formatting.py
@@ -2,10 +2,15 @@ import sys
 import requests
 
 print(sys.executable)
+
+
 def greet(who_to_greet):
     greeting = "hello, {}".format(who_to_greet)
     return greeting
+
+
 r = requests.get("https://coreyms.com")
 print(r.status_code)
 
-
+hello = "hello"
+print(hello)

2f2189fd5110cdcd137676d55c1e84b9265823df
piersonphua@hotmail.com <piersonphua@hotmail.com>
2022-07-30 11:00:23 +0800

    initial commit

---
+++ formatting.py
@@ -0,0 +1,11 @@
+import sys
+import requests
+
+print(sys.executable)
+def greet(who_to_greet):
+    greeting = "hello, {}".format(who_to_greet)
+    return greeting
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+
+
